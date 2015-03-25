2b43087e2367bf4ea608c369002981df247b7d21
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-03-25 17:08:27 -0400

    Scripts will be written as DJango templates: to finish

---
+++ src/eqsans/scripts/reduce.py
@@ -0,0 +1,83 @@
+# EQSANS reduction script
+import mantid
+from mantid.simpleapi import *
+
+from reduction_workflow.instruments.sans.sns_command_interface import *
+
+config = ConfigService.Instance()
+config['instrumentName']='EQSANS'
+
+{% if mask_file %}
+mask_ws = Load(Filename='{{ mask_file }}')
+ws, masked_detectors = ExtractMask(InputWorkspace=mask_ws, OutputWorkspace='__edited_mask')
+detector_ids = [int(i) for i in masked_detectors]
+{% endif %}
+
+EQSANS()
+SolidAngle(detector_tubes=True)
+TotalChargeNormalization()
+
+{% if absolute_scale_factor %}
+SetAbsoluteScale({{ absolute_scale_factor }})
+{% endif %}
+
+AzimuthalAverage(n_bins=100, n_subpix=1, log_binning=False) # TODO
+IQxQy(nbins=100) # TODO
+OutputPath('{{ output_path }}')  
+        
+UseConfigTOFTailsCutoff(True)
+UseConfigMask(True)
+Resolution(sample_aperture_diameter={{ sample_aperture_diameter }})
+PerformFlightPathCorrection(True)
+
+{% if mask_file %}     
+MaskDetectors(detector_ids)
+{% endif %}
+
+
+{% if dark_current_run %}
+DarkCurrentFile='{{dark_current_run}}'
+{% endif %}
+
+{% if fit_direct_beam %}
+DirectBeamCenter('{{fit_direct_beam}}') 
+{% else %}
+SetBeamCenter({{beam_center_x}},{{beam_center_y}})
+{% endif %}
+
+{% if perform_sensitivity %}            
+SensitivityCorrection({{sensitivity_file}},
+                      min_sensitivity={{sensitivity_min}},
+                      max_sensitivity={{sensitivity_max}},
+                      use_sample_dc=True)
+{% else %}
+NoSensitivityCorrection()
+{% endif %}
+
+{% if beam_radius %}
+beam_radius={{beam_radius}}
+{% else %}
+beam_radius={{cls.base_fields['beam_radius'].initial}}
+{% endif %}
+
+DirectBeamTransmission({{transmission_sample}}, {{transmission_empty}}, beam_radius={{beam_radius}})
+        
+ThetaDependentTransmission({{theta_dependent_correction}})
+
+{% if nickname %}
+AppendDataFile(['{{data_file}}'], '{{nickname}}')
+{% else %}
+AppendDataFile(['{{data_file}}'])
+{% endif %}    
+
+CombineTransmissionFits({{fit_frames_together}})
+
+{% if subtract_background %}
+Background('{{background_file}}')
+BckThetaDependentTransmission({{theta_dependent_correction}})
+BckCombineTransmissionFits({{fit_frames_together}})
+BckDirectBeamTransmission('{{background_transmission_sample}}', '{{background_transmission_empty}}', beam_radius=beam_radius)
+{% endif %}    
+        
+SaveIq(process='None')
+Reduce()
